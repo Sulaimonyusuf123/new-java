ad095f5e681184fd448e83f62d2388b80ccfe5f4
olamilekan <olanrewajuolamilekan39@gmail.com>
2023-05-03 10:03:51 +0100

    javascripts

---
+++ new.js
@@ -0,0 +1,10 @@
+function sumUp () {
+let olaitan_1 = Number(document.getElementById('olaitan1').value);
+let olaitan_2 = Number(document.getElementById('olaitan2').value);
+let olaitan_3 = document.getElementById('olaitan3').value = olaitan_1 / olaitan_2;
+let olaitan_4 = document.getElementById('olaitan4').value = olaitan_1 + olaitan_2 + olaitan_3;
+
+
+
+
+}
